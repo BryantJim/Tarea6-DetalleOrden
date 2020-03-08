b62d531c903f6d0155a5af4945cb58ca45c51d12
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-07 20:43:18 -0400

    Base de datos y migracion

---
+++ DetalleOrden/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DetalleOrden.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace DetalleOrden.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Ordenes> Ordenes { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Database\DetalleOrden.db");
+        }
+    }
+}
