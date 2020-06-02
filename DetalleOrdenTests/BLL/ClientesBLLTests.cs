808732d093966ade6bd936f544d76144f5e29f46
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-06-01 20:42:43 -0400

    Test

---
+++ DetalleOrdenTests/BLL/ClientesBLLTests.cs
@@ -0,0 +1,42 @@
+﻿using DetalleOrden.BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DetalleOrden.BLL.Tests
+{
+    [TestClass()]
+    public class ClientesBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
